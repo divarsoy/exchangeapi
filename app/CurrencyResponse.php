8cb68121cae4ce8688d1041dfb1e1830b7cae09a
Dag Ivarsoy <dagivarsoy@gmail.com>
2020-04-12 22:08:30 +0100

    Add Response types

---
+++ app/CurrencyResponse.php
@@ -0,0 +1,26 @@
+<?php declare(strict_types=1);
+
+namespace App;
+
+class CurrencyResponse implements iResponse
+{
+    private $error;
+    private $amount;
+    private $fromCache;
+
+    public function __construct(int $error, float $amount, int $fromCache)
+    {
+        $this->error = $error;
+        $this->amount = $amount;
+        $this->fromCache = $fromCache;
+    }
+
+    public function generateResponse():array
+    {
+        return [
+            "error" => $this->error,
+            "amount" => number_format($this->amount,2,'.',''),
+            "fromCache" => $this->fromCache
+        ];
+    }
+}
---
+++ app/ErrorResponse.php
@@ -0,0 +1,22 @@
+<?php declare(strict_types=1);
+
+namespace App;
+
+class ErrorResponse implements iResponse
+{
+    private $error;
+    private $msg;
+
+    public function __construct(int $error, string $msg)
+    {
+        $this->error = $error;
+        $this->msg = $msg;
+    }
+
+    public function generateResponse():array{
+        return [
+            "error" => $this->error,
+            "msg" => $this->msg
+        ];
+    }
+}
